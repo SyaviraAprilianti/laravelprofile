960a394d4de511c8bb688299339436a4ff30c442
SyaviraAprilianti <syavira890@gmail.com>
2020-07-27 15:09:13 +0700

    Program Laravel

---
+++ blog/resources/views/home.blade.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <!-- link css -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<style>
+body{
+  background-color:#5F9EA0;
+}
+</style>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="##profil">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link" href="#experience">Experience</a>
+                <a class="nav-item nav-link" href="#portofolio">Portofolio</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="jumbotron" bg-colour="#E6E6FA">
+    
+    <center>
+    <img src="foto/pira.jpg" width="180px" height="180px" class="rounded-circle">
+    <h1 class="display-5">Syavira Aprilianti</h1>
+    <p class="lead">Student</p>
+    <p>SMK Taruna Bhakti</p>
+    </center>
+    
+    </div>
+
+    <div class="container-fluid bg" >
+    <div class="row">
+        <div class="col text-center" bg-colour="#5F9EA0">
+            <h2 id="profil" style="font-family: Candara; font-size: 40px"><b>Profile</b></h2><br>
+        </div>
+    </div>
+        <p class="col text-center" style="font-size: 20px; font-family: Candara;" >
+        Halo, Nama saya Syavira Aprilianti. Saya lahir di Bogor pada 24 April 2003. Saya seorang Muslim. Rumah saya terletak di Cilodong, Bendungan RT 02/02, Kelurahan Cilodong, Kecamatan Cilodong, Kota Depok. Saya putri dari seorang ayah bernama Alamsyah dan seorang ibu bernama Yuspuanti, saya adalah  anak satu-satunya . impian saya adalah menjadi polisi. Saya sekarang kelas XII RPL 1. Saya bergabung dengan kegiatan ekstrakurikuler, Paskibra, dan Pramuka.
+
+        Hobi saya adalah Membaca Buku, Menonton Film, Mendengarkan musik. lagu favorit saya adalah Skylar Gray Everything I Need. warna favorit saya adalah biru, dan abu-abu.
+        </p>
+    </div>
+    <br>
+    <div class="container">
+    <div class="col text-center">
+        <h3 id="experience" style="font-size: 40px; font-family: Candara;"><b>EXPERIENCE</b></h3>
+        <br>
+  <div class="row">
+  
+  <div class="col md-4">
+            <div class="card">
+                <img src="foto/ngaji.jpg" class="card-img-top" height="350px">
+                    <div class="card-body" style="font-family: Candara; font-size: 35px"><b><h3>Tutor Sebaya RPL</b></h3>
+                        <p class="card-text" style="font-size: 23px; font-family: Candara;">Pembelajaran Tutor Sebaya RPL ini mempelajari bagaimana cara pembuatan aplikasi dan di pandu oleh  kakak kelas RPL untuk membuat aplikasi mengaji</p>
+                    </div>
+                </div>
+            </div>
+    <div class="col md-4">
+        <div class="card">
+            <img src="foto/kunjin.jpg" class="card-img-top" width="100px" height="350px">
+                <div class="card-body" style="font-family: Candara; font-size: 35px"><b><h3>Kunjungan Industri (Kunjin)</b></h3>
+                    <p class="card-text" style="font-size: 23px; font-family: Candara;">Kunjungan Industri pada tanggal 4 Januari 2019 di PT. CODE.ID yang beralamat di APL Tower, RT.9/RW.5, Tj. Duren Sel., Jakarta, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11470</p>
+                </div>
+            </div>
+        </div>
+
+        
+        </div>
+     </div>
+</div>
+      <br><br><br>
+
+<br><br><br>
+
+<div class="container">
+  <div class="row">
+    <div class="col text-center">
+      <h2 id="portofolio" style="font-family: Candara; font-size: 40px"><b>Portofolio</h2></b><br>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col md-4">
+        <div class="card">
+          <img src="foto/submission.png" class="card-img-top" height="350px">
+        <div class="card-body" style="font-family: Candara"><b><h3>Tugas Submission</b></h3>
+          <p class="card-text" style="font-size: 23px; font-family: Candara;">Project ini dibuat ketika saya kelas X mengikuti belajar dicoding dan saya memilih mengambil kelas Pemrograman Dasar dan membuat tampilan seperti pada gambar menggunakan html</p>
+        </div>
+        </div>
+    </div>
+
+    <div class="col md-4">
+        <div class="card col md-4">
+          <img src="foto/sertifikat.png" class="card-img-top" height="350px">
+        <div class="card-body" style="font-family: Candara;"><b><h3>Sertifikasi Dicoding</b></h3>
+          <p class="card-text"style="font-family: Candara; font-size: 23px">Sertifikat ini saya dapatkan karena telah menyelesaikan tugas Submission Web Fundamental dan  telah berhasil membuat website biodata diri dengan memanfaatkan element div dan Stylesheet dalam menyusun halaman.</p>
+        </div>
+        </div>
+    </div>
+
+    <div class="col md-4">
+        <div class="card">
+          <img src="foto/pbo.png" class="card-img-top" height="350px">
+        <div class="card-body"  style="font-family: Candara;"><b><h3>Tugas Membuat Aplikasi Penjualan Buku sederhana (PIRSMEDIA)</b></h3>
+          <p class="card-text"  style="font-family: Candara; font-size: 23px">Tugas ini dibuat ketika saya kelas 11 pada mata pelajaran PBO (Pemrograman Berorientasi Objek) dan PIRSMEDIA ini adalah aplikasi penjualan buku sederhana yang masih menggunakan JavaScript dan Html untuk  menjalankan perintah CRUD yaitu Create, Read, Update, dan Delete.</p>
+        </div>
+        </div>
+    </div>
+  </div>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ blog/resources/views/myblog.blade.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    
+    <title>My Blog</title>
+  </head>
+  <body>
+
+  <style>
+      .jumbotron{
+          background: url(img/nct.jpg) center center;
+      }
+      
+  </style>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+  <div class="navbar-nav"> 
+<a class="nav-item nav-link active" href="#profil" style="font-size: 35px; color: green" >Profile <span class="sr-only">(current)</span></a>
+  <a class="nav-item nav-link" href="#experience" style="font-size: 35px; color: green">Experience</a>
+    <a class="nav-item nav-link" href="#portofolio" style="font-size: 35px; color: green">Portofolio</a>
+      </div>
+    </div>
+  </nav>
+
+<div class="jumbotron">
+    <center>
+    <img src="img/avatar4.png" width="180px" class="rounded-circle">
+  <h1 class="display-5" style="font-family: Candara; color: white">Nila Nurazizah</h1>
+  <p class="lead" style="font-size: 30px; font-family: Candara; color: white">
+        (Student)
+</p>
+  <p style="font-size: 30px; font-family: Candara; color: white">SMK Taruna Bhakti</p>
+  </center>
+</div>
+
+ <div class="container-fluid">
+ <div class="row">
+    <div class="col text-center">
+      <h2 id="profil" style="font-family: Candara; font-size: 40px"><b>PROFILE</b></h2><br>
+    </div>
+  </div>
+    <p class="col text-left" style="font-size: 30px; font-family: Candara; ">
+    Nama saya Nila Nurazizah. Saya lahir di Depok, 14 Juni 2003 dan Umur saya 17 Tahun. 
+    Saya bersekolah di SMK Taruna Bhakti Depok dan saya sekarang berada di kelas 12 jurusan RPL (Rekayasa Perangkat Lunak).
+    Alamat rumah saya di Kp.Sindangkarsa RT 01/ RW 02 No. 40 Kel. Sukamaju Baru  Kec. Tapos Depok.
+    </p>
+    <br>
+  </div>
+  <br>
+
+<br>
+  <div class="container">
+    <div class="col text-center">
+        <h3 id="experience" style="font-size: 40px; font-family: Candara;"><b>EXPERIENCE</b></h3>
+        <br>
+  <div class="row">
+    <div class="col md-4">
+        <div class="card">
+            <img src="img/kunjin.jpg" class="card-img-top">
+                <div class="card-body" style="font-family: Candara; font-size: 35px"><b><h3>Kunjungan Industri (Kunjin)</b></h3>
+                    <p class="card-text" style="font-size: 23px; font-family: Candara;">Kunjungan Industri pada tanggal 4 Januari 2019 di PT Indonesia Comnets Plus (ICON+), yang berlokasi di Jl. Damai No.100, Gandul, Kec. Cinere, Kota Depok, Jawa Barat 16514</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col md-4">
+            <div class="card">
+                <img src="img/tutor.png" class="card-img-top">
+                    <div class="card-body" style="font-family: Candara; font-size: 35px"><b><h3>Tutor Sebaya RPL</b></h3>
+                        <p class="card-text" style="font-size: 23px; font-family: Candara;">Pembelajaran Tutor Sebaya RPL ini mempelajari bagaimana cara pembuatan aplikasi dengan kakak kelas RPL dan aplikasi yang di buat itu adalah aplikasi mengaji</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </div>
+</div>
+      <br><br><br>
+
+<br><br><br>
+
+<div class="container">
+  <div class="row">
+    <div class="col text-center">
+      <h2 id="portofolio" style="font-family: Candara; font-size: 40px"><b>Portofolio</h2></b><br>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col md-4">
+        <div class="card">
+          <img src="img/submision.png" class="card-img-top">
+        <div class="card-body" style="font-family: Candara"><b><h3>Tugas Submission</b></h3>
+          <p class="card-text" style="font-size: 23px; font-family: Candara;">Project ini dibuat ketika saya mengikuti belajar dicoding dan kelas yang saya ambil yaitu Pemrograman Dasar.</p>
+        </div>
+        </div>
+    </div>
+
+    <div class="col md-4">
+        <div class="card col md-4">
+          <img src="img/sertifikat.png" class="card-img-top">
+        <div class="card-body" style="font-family: Candara;"><b><h3>Sertifikasi Dicoding</b></h3>
+          <p class="card-text"style="font-family: Candara; font-size: 23px">Sertifikat ini Saya Dapatkan Karena Sudah Menyelesaikan Tugas Submission Pada Pembelajaran Dasar Pemrograman Web.</p>
+        </div>
+        </div>
+    </div>
+
+    <div class="col md-4">
+        <div class="card">
+          <img src="img/laptop.png" class="card-img-top">
+        <div class="card-body"  style="font-family: Candara;"><b><h3>Tugas Membuat Aplikasi Penjualan Laptop Sederhana</b></h3>
+          <p class="card-text"  style="font-family: Candara; font-size: 23px">Tugas ini dibuat ketika saya kelas 11 pada mata pelajaran PBO (Pemrograman Berorientasi Objek) dan tugas ini masih menggunakan JavaScript dan Html</p>
+        </div>
+        </div>
+    </div>
+  </div>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
